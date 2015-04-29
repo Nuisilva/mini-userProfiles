f31303bebde504e1e3342a80e131ac6a05eddfdb
Nui Silva <sigbin2000@gmail.com>
2015-04-29 06:31:42 -0600

    Phinished

---
+++ controller.js
@@ -0,0 +1,12 @@
+var app = angular.module('userProfiles');
+
+app.controller('MainController', function($scope, mainService) {
+  
+  var getUsers = function() {
+    $scope.users = mainService.getUsers();
+  }
+  
+  getUsers();
+
+
+});
